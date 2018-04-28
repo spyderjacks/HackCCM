b6e40ab2025eec9f86af6892829d6fff6f8ee624
Michael Sydor <spyderjacks@gmail.com>
2018-04-27 21:55:40 -0400

    Added for non-NOOBs distros

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+#
+# setup a raspberry pi for HackCCM, camera and messaging tutorials
 
+# assuming unknown state
+#
+sudo apt-get install git-core
+sudo apt-get install python3-picamera 
+sudo apt-get install python3-pip
+
+# assuming NOOBS distribution
+#
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install libav-tools
@@ -7,6 +17,10 @@ sudo apt-get install x264
 sudo apt-get install realvnc-vnc-server
 sudo systemctl enable vncserver-x11-serviced.service
 sudo pip3 install paho-mqtt
+
+sudo apt-get install realvnc-vnc-server
+sudo apt-get install realvnc-vnc-viewer
+sudo systemctl enable vncserver-x11-serviced.service
 
 
 
